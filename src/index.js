b028c6379b25417ad41b7e6fd0e2d7ad13cca00c
Yarden Samorai <yardensamorai6@gmail.com>
2025-03-22 13:01:54 +0200

    fixing index.js

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ClerkProvider publishableKey={phublishKey} afterSignOutUrl="/">
+      <App />
  </ClerkProvider>
   </React.StrictMode>
 );
